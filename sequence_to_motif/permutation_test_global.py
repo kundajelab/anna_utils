bfa07d5de137bbc97b0a9041d8e8851095b6355b
anna shcherbina <annashcherbina@gmail.com>
2017-02-23 15:44:31 -0800

    support for scanning multiple motifs at once, support for frequency inputs (in addition to pwm)

--- sequence_to_motif/permutation_test_global.py
+++ sequence_to_motif/permutation_test_global.py
@@ -23,14 +23,16 @@ def totuple(a):
 def parse_args():
     parser=argparse.ArgumentParser(description='permutation test for a specified motif, globally')
     parser.add_argument("--foreground_bed",help="bed file containing the foreground region")
-    parser.add_argument("--reference",help="reference fasta file")
+    parser.add_argument("--reference",help="reference fasta file for whole genome. i.e. hg19.fa")
     parser.add_argument("--out_prefix",help="output file prefix")
-    parser.add_argument("--permuted_reference",help="permuted genome in bins of 1kb")
-    parser.add_argument("--pwm",help="pwm file for motif of interest")
+    parser.add_argument("--permuted_reference",help="permuted genome in bins of 1kb, also a fasta file i.e. hg19.permuted.fa")
+    parser.add_argument("--pwm",type=str,nargs="+",help="list of pwm files (separated by space)")
     parser.add_argument("--p_val",help="p-value cutoff for motif calling")
-    parser.add_argument('--foreground_freqs',help='file containing the background allele frequencies,will be computed on the fly if not provided')
+    parser.add_argument('--foreground_freqs',help='file containing the background allele frequencies,will be computed on the fly if not provided,allele frequencies are tab-delimited in order of A, C, G, T. i.e. 0.22 0.28 0.28 0.22')
     parser.add_argument('--test_to_perform',help='one of hist,fdr,score_cutoff')
     parser.add_argument('--fdr_thresh',default=0.2)
+    parser.add_argument('--freqs',help="use this flag if you are providing a matrix of frequencies rather than a PWM",action='store_true')
+    parser.add_argument('--pseudocount',help="pseudocount to add when providing a matrix of frequency values rather than a PWM",default=1e-4)
     return parser.parse_args() 
 
 #parses the provided foreground bed file
@@ -77,12 +79,6 @@ def main():
     permuted_reference=pysam.FastaFile(args.permuted_reference) 
     print("opened references") 
 
-    #load the pwm 
-    matrix_file_name=args.pwm
-    matrix=totuple(np.transpose(np.loadtxt(matrix_file_name,skiprows=1)))
-    motif_name=matrix_file_name.split('.')[0]
-    print("read in motif of interest:"+str(motif_name))
-
     #read in the foreground & compute base frequencies 
     foreground=parse_foreground(args.foreground_bed)
     if args.foreground_freqs==None:
@@ -91,127 +87,141 @@ def main():
         fg_freqs=[float(i) for i in open(args.foreground_freqs,'r').read().strip().split('\t')]
     print('computed base frequencies in foreground')
 
-    threshold=MOODS.tools.threshold_from_p(matrix,fg_freqs,float(args.p_val))
-    #pdb.set_trace() 
+    #load the pwm 
+    matrix_file_names=args.pwm
+    matrices=[]
+    motif_names=[matrix_file_name.split('.')[0] for matrix_file_name in matrix_file_names]
+    for matrix_file_name in matrix_file_names:
+        if args.freqs==False:
+            #input matrices are in pwm format
+            matrices.append(totuple(np.transpose(np.loadtxt(matrix_file_name,skiprows=1))))
+        else:
+            #input matrices are provided as frequency tables
+            matrices.append(MOODS.parsers.pfm_to_log_odds(matrix_file_name,fg_freqs,args.pseudocount))
+    print("read in motifs of interest:"+str(motif_names))
+
+    thresholds=[MOODS.tools.threshold_from_p(matrix,fg_freqs,float(args.p_val)) for matrix in matrices]
+
     #initalize the MOODS scanner 
     scanner = MOODS.scan.Scanner(7)
-    scanner.set_motifs([matrix], fg_freqs, [threshold],)
+    scanner.set_motifs(matrices, fg_freqs, thresholds)
     print("initialized scanner...")
     
     #perform scan of original foreground, get histogram of hit scores
     distribution_original=dict()
     distribution_permuted=dict()
-    list_original=[]
-    list_permuted=[] 
-    num_hits_original=0
-    num_hits_permuted=0
+    num_hits_original=dict() 
+    num_hits_permuted=dict() 
 
+    for motif_index in range(len(motif_names)):
+        distribution_original[motif_index]=dict()
+        distribution_permuted[motif_index]=dict()
+        num_hits_original[motif_index]=0
+        num_hits_permuted[motif_index]=0
+        
     #scan each original & permuted region in the foreground
     num_entries=str(len(foreground))
-    parsed=0 
     for entry in foreground:
         seq=reference.fetch(entry[0],int(entry[1]),int(entry[2]))
         seq_permuted=permuted_reference.fetch(entry[0],int(entry[1]),int(entry[2]))
 
         #update the distributions of motif hit scores 
-        hits_original=scanner.scan(seq)[0]
-        scores_original=[round(hit.score,1) for hit in hits_original]
-        num_hits_original+=len(scores_original)
-        #list_original[0:0]=scores_original 
-        distribution_original=update_distribution(distribution_original,scores_original)
+        hits_original=scanner.scan(seq)
+        hits_permuted=scanner.scan(seq_permuted)
         
-        hits_permuted=scanner.scan(seq_permuted)[0]
-        scores_permuted=[round(hit.score,1) for hit in hits_permuted]
-        num_hits_permuted+=len(scores_permuted)
-        #list_permuted[0:0]=scores_permuted 
-        distribution_permuted=update_distribution(distribution_permuted,scores_permuted) 
-        parsed+=1
-        if parsed%1000==0:
-            print str(parsed)+"/"+num_entries 
+        for motif_index in range(len(hits_original)):
+            motif_hits=hits_original[motif_index]
+            scores_original=[round(hit.score,1) for hit in motif_hits]
+            num_hits_original[motif_index]+=len(scores_original)
+            distribution_original[motif_index]=update_distribution(distribution_original[motif_index],scores_original)
+
+            motif_hits_permuted=hits_permuted[motif_index]
+            scores_permuted=[round(hit.score,1) for hit in motif_hits_permuted]
+            num_hits_permuted[motif_index]+=len(scores_permuted)
+            distribution_permuted[motif_index]=update_distribution(distribution_permuted[motif_index],scores_permuted)
     print("completed scanning")
-    
-    #write out the histograms 
-    outf_original=open(args.out_prefix+".original",'w')
-    score_keys_original=distribution_original.keys()
-    score_keys_original.sort()
-    for key in score_keys_original:
-        outf_original.write(str(key)+'\t'+str(distribution_original[key])+'\n')
-        
-    outf_permuted=open(args.out_prefix+".permuted",'w')
-    score_keys_permuted=distribution_permuted.keys()
-    score_keys_permuted.sort() 
-    for key in score_keys_permuted:
-        outf_permuted.write(str(key)+'\t'+str(distribution_permuted[key])+'\n')
 
-    #select the specific test to perform 
-    if args.test_to_perform=="hist": 
-        print("generating lists for histogram:") 
-        merged_original=[[i]*j for (i,j) in distribution_original.items()]
-        merged_permuted=[[i]*j for (i,j) in distribution_permuted.items()]
-        list_original=list(itertools.chain.from_iterable(merged_original))
-        list_permuted=list(itertools.chain.from_iterable(merged_permuted))
-
-        plt.hist(list_original,bins=20,histtype='stepfilled',normed=False,color='b',label='hg19 original')
-        plt.hist(list_permuted,bins=20,histtype='stepfilled',normed=False,color='r',alpha=0.5,label='local genome permutations')
-        plt.title(motif_name+" at p-thresh="+str(args.p_val))
-        plt.xlabel("Motif Score")
-        plt.ylabel("Number of hits")
-        plt.legend()
-        fig = matplotlib.pyplot.gcf()
-        fig.set_size_inches(4, 3)
-        plt.savefig(motif_name.split('/')[-1]+"."+str(args.p_val)+".hist.png",dpi=100)
-    elif args.test_to_perform=="fdr":
-        fdr_dict=dict()
-        score_keys=list(set(distribution_original.keys()).union(set(distribution_permuted.keys())))
-        for key in score_keys:
-            original_hits=0
-            permuted_hits=0
-            if key in distribution_original: 
-                original_hits=distribution_original[key]
-            if key in distribution_permuted: 
-                permuted_hits=distribution_permuted[key]
-            fdr_val=permuted_hits/(1.0*(permuted_hits+original_hits))
-            fdr_dict[key]=fdr_val
-        print("found fdr!")
-        plt.scatter(fdr_dict.keys(),fdr_dict.values(),color="blue")
-        plt.title(motif_name+" at p-thresh="+str(args.p_val))
-        plt.xlabel("Motif Score")
-        plt.ylabel("FDR: shuffled_hits/(shuffled_hits+original_hits)")
-        #fig = matplotlib.pyplot.gcf()
-        #fig.set_size_inches(4, 3)
-        #pdb.set_trace() 
-        plt.savefig(motif_name.split('/')[-1]+"."+str(args.p_val)+".fdr.png")#,dpi=300)
-    elif args.test_to_perform=="score_cutoff":
-        min_score_for_good_fdr=float("inf")
-        score_keys=list(set(distribution_original.keys()).union(set(distribution_permuted.keys())))
-        for key in score_keys:
-            original_hits=0
-            permuted_hits=0
-            if key in distribution_original: 
-                original_hits=distribution_original[key]
-            if key in distribution_permuted: 
-                permuted_hits=distribution_permuted[key]
-            fdr_val=permuted_hits/(1.0*(permuted_hits+original_hits))
-            if fdr_val < args.fdr_thresh:
-                if key < min_score_for_good_fdr:
-                    min_score_for_good_fdr=key
-        #we have found the score threshold, now scan the non-permuted genome at that threshold & compute the number of hits.
-        scanner = MOODS.scan.Scanner(7)
-        scanner.set_motifs([matrix], fg_freqs, [min_score_for_good_fdr],)
-        num_hits_above_min_score=0
-        for entry in foreground:
-            seq=reference.fetch(entry[0],int(entry[1]),int(entry[2]))
-            hits_original=scanner.scan(seq)[0]
-            num_hits_above_min_score+=len(hits_original)
-            
-        outf=open(motif_name.split('/')[-1]+"."+str(args.p_val)+"."+str(args.fdr_thresh)+".tsv",'w')
+    if args.test_to_perform=="score_cutoff":
+        outf=open("score_cutoffs."+str(args.p_val)+"."+str(args.fdr_thresh)+".tsv",'w')
         outf.write('Motif\tP-val\tFDR-thresh\tScore-Cutoff\tNumHitsAbovesCoreCutoff\n')
-        outf.write(motif_name.split('/')[-1]+'\t'+str(args.p_val)+'\t'+str(args.fdr_thresh)+'\t'+str(min_score_for_good_fdr)+'\t'+str(num_hits_above_min_score)+'\n')        
+
+    for motif_index in range(len(motif_names)): 
+        #write out the histograms 
+        outf_original=open(motif_names[motif_index]+"."+args.out_prefix+".original",'w')
+        score_keys_original=distribution_original[motif_index].keys()
+        score_keys_original.sort()
+        for key in score_keys_original:
+            outf_original.write(str(key)+'\t'+str(distribution_original[motif_index][key])+'\n')
         
-    else:
-        #we want to find the score cutoff         
-        print("invalid test_to_perform specified! Must be one of hist,fdr,score_cutoff")
-        exit()
+        outf_permuted=open(motif_names[motif_index]+"."+args.out_prefix+".permuted",'w')
+        score_keys_permuted=distribution_permuted[motif_index].keys()
+        score_keys_permuted.sort() 
+        for key in score_keys_permuted:
+            outf_permuted.write(str(key)+'\t'+str(distribution_permuted[motif_index][key])+'\n')
+
+        #select the specific test to perform 
+        if args.test_to_perform=="hist": 
+            print("generating lists for histogram:") 
+            merged_original=[[i]*j for (i,j) in distribution_original[motif_index].items()]
+            merged_permuted=[[i]*j for (i,j) in distribution_permuted[motif_index].items()]
+            list_original=list(itertools.chain.from_iterable(merged_original))
+            list_permuted=list(itertools.chain.from_iterable(merged_permuted))
+
+            plt.hist(list_original,bins=20,histtype='stepfilled',normed=False,color='b',label='hg19 original')
+            plt.hist(list_permuted,bins=20,histtype='stepfilled',normed=False,color='r',alpha=0.5,label='local genome permutations')
+            plt.title(motif_names[motif_index]+" at p-thresh="+str(args.p_val))
+            plt.xlabel("Motif Score")
+            plt.ylabel("Number of hits")
+            plt.legend()
+            fig = matplotlib.pyplot.gcf()
+            fig.set_size_inches(4, 3)
+            plt.savefig(motif_names[motif_index].split('/')[-1]+"."+str(args.p_val)+".hist.png",dpi=100)
+        elif args.test_to_perform=="fdr":
+            fdr_dict=dict()
+            score_keys=list(set(distribution_original[motif_index].keys()).union(set(distribution_permuted[motif_index].keys())))
+            for key in score_keys:
+                original_hits=0
+                permuted_hits=0
+                if key in distribution_original[motif_index]: 
+                    original_hits=distribution_original[motif_index][key]
+                if key in distribution_permuted[motif_index]: 
+                    permuted_hits=distribution_permuted[motif_index][key]
+                fdr_val=permuted_hits/(1.0*(permuted_hits+original_hits))
+                fdr_dict[key]=fdr_val
+            print("found fdr!")
+            plt.scatter(fdr_dict.keys(),fdr_dict.values(),color="blue")
+            plt.title(motif_names[motif_index]+" at p-thresh="+str(args.p_val))
+            plt.xlabel("Motif Score")
+            plt.ylabel("FDR: shuffled_hits/(shuffled_hits+original_hits)")
+            plt.savefig(motif_names[motif_index].split('/')[-1]+"."+str(args.p_val)+".fdr.png")#,dpi=300)
+        elif args.test_to_perform=="score_cutoff":
+            min_score_for_good_fdr=float("inf")
+            score_keys=list(set(distribution_original[motif_index].keys()).union(set(distribution_permuted[motif_index].keys())))
+            for key in score_keys:
+                original_hits=0
+                permuted_hits=0
+                if key in distribution_original[motif_index]: 
+                    original_hits=distribution_original[motif_index][key]
+                if key in distribution_permuted[motif_index]: 
+                    permuted_hits=distribution_permuted[motif_index][key]
+                fdr_val=permuted_hits/(1.0*(permuted_hits+original_hits))
+                if fdr_val < args.fdr_thresh:
+                    if key < min_score_for_good_fdr:
+                        min_score_for_good_fdr=key
+            #we have found the score threshold, now scan the non-permuted genome at that threshold & compute the number of hits.
+            scanner = MOODS.scan.Scanner(7)
+            scanner.set_motifs([matrices[motif_index]],fg_freqs, [min_score_for_good_fdr],)
+            num_hits_above_min_score=0
+            for entry in foreground:
+                seq=reference.fetch(entry[0],int(entry[1]),int(entry[2]))
+                hits_original=scanner.scan(seq)[0]
+                num_hits_above_min_score+=len(hits_original)
+            outf.write(motif_names[motif_index].split('/')[-1]+'\t'+str(args.p_val)+'\t'+str(args.fdr_thresh)+'\t'+str(min_score_for_good_fdr)+'\t'+str(num_hits_above_min_score)+'\n')        
+
+        else:
+            #we want to find the score cutoff         
+            print("invalid test_to_perform specified! Must be one of hist,fdr,score_cutoff")
+            exit()
         
     
 if __name__=="__main__":
---
+++ sequence_to_motif/permutation_test_global_inK562_multi_pwm.sh
@@ -0,0 +1,8 @@
+for pval in 0.0001 0.00005 0.00001
+do
+    for thresh in 0.01 0.05 0.1 0.15 0.25
+    do
+	python permutation_test_global.py --foreground_bed ENCSR000EPC.K562_Leukemia_Cells.UW_Stam.DNase-seq_rep1-pr.IDR0.1.filt.narrowPeak --reference hg19/hg19.genome.fa --out_prefix fdr0.01.pval.$pval --foreground_freqs k562.test.ATF3.0.01.foreground_freqs.txt  --permuted_reference hg19/hg19.shuffled.fa --pwm pwm/ATF3_HUMAN.H10MO.A.pwm pwm/TAL1_HUMAN.H10MO.A.pwm pwm/CREB1_HUMAN.H10MO.A.pwm pwm/CTCF_HUMAN.H10MO.A.pwm pwm/E2F4_HUMAN.H10MO.A.pwm pwm/ELF1_HUMAN.H10MO.A.pwm pwm/GABPA_HUMAN.H10MO.A.pwm pwm/GATA1_HUMAN.H10MO.A.pwm pwm/MAX_HUMAN.H10MO.A.pwm pwm/IRF1_HUMAN.H10MO.A.pwm --p_val $pval --test_to_perform score_cutoff --fdr_thresh $thresh
+	echo "done!: $pval $thresh"
+    done
+done
