f6227e2059af98093adca8c25b20fbdda5e98e98
anna shcherbina <annashcherbina@gmail.com>
2017-03-23 11:53:05 -0700

    framework qc files

---
+++ framework_qc/momma_dragonn_config_files/README.txt
@@ -0,0 +1,3 @@
+The yaml files that have "master" pre-prended to them are designed to work with the Master branch of the momma_dragonn repository.
+The yaml files that have "anna" pre-prended will work with anna_dev
+
--- framework_qc/runModelPureKeras.sh
+++ framework_qc/runModelPureKeras.sh
@@ -13,8 +13,8 @@ basedir=/srv/scratch/annashch/deeplearning/dmso
 #functional keras model w/ 1e-6 regularization & normalized data
 #CUDA_VISIBLE_DEVICES=3 python runModelPureKeras.py --train_path inputs/s_by_m_by_pos/normalized.dmso.train.hdf5 --valid_path inputs/s_by_m_by_pos/normalized.dmso.validate.hdf5 --model_output_file purekeras.NEW.functional.1e-6.hdf5 --model_builder dmso_model_regularized.py --w0_file w0.txt --w1_file w1.txt --batch_size 1000
 
-
-THEANO_FLAGS="device=gpu0" python runModelPureKerasOld.py --train_path $basedir/inputs/s_by_m_by_pos/normalized.dmso.train.hdf5 --valid_path $basedir/inputs/s_by_m_by_pos/normalized.dmso.validate.hdf5 --model_output_file $basedir/purekeras.OLD.graph.1e-6.hdf5 --model_builder dmso_legacy_graph_modelOld.py --w0_file $base_dir/w0.txt --w1_file $base_dir/w1.txt --batch_size 1000
+#Old keras compatibility 
+#CUDA_VISIBLE_DEVICES=0 python runModelPureKerasOld.py --train_path $basedir/inputs/s_by_m_by_pos/normalized.dmso.train.hdf5 --valid_path $basedir/inputs/s_by_m_by_pos/normalized.dmso.validate.hdf5 --model_output_file $basedir/purekeras.OLD.graph.1e-6.hdf5 --model_builder dmso_legacy_graph_modelOld.py --w0_file $base_dir/w0.txt --w1_file $base_dir/w1.txt --batch_size 1000
 
 #LEGACY GRAPH MODEL
 #CUDA_VISIBLE_DEVICES=7 python runModelPureKeras.py --train_path inputs/s_by_m_by_pos/normalized.dmso.train.hdf5 --valid_path inputs/s_by_m_by_pos/normalized.dmso.validate.hdf5 --model_output_file purekeras.NEW.graph.1e-6.hdf5 --model_builder dmso_legacy_graph_model.py --w0_file w0.txt --w1_file w1.txt --batch_size 1000
