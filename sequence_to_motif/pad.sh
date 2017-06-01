60f6dab9bf3f004c555126c4d4d0b98eae0ed531
anna shcherbina <annashcherbina@gmail.com>
2017-05-31 17:24:26 -0700

    shell files

---
+++ sequence_to_motif/pad.sh
@@ -0,0 +1,3 @@
+python pad.py --input_bed /srv/scratch/shared/surya/raunaq/chip-nexus/data-stowers/semi_processed/Sox2_1_3/out/peak/idr/optimal_set/Sox2_1_3_rep1-pr.IDR0.05.filt.12-col.bed \
+       --desired_length 1000\
+       --output_bed Sox2_1_3_rep1-pr.IDR0.05.filt.12-col.bed
