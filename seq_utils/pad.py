19a012cb62fb5bc08cdbcdaf0c8c43a9be86d0b4
anna shcherbina <annashcherbina@gmail.com>
2018-10-14 22:03:37 -0700

    fixed padding and sampling

--- seq_utils/pad.py
+++ seq_utils/pad.py
@@ -6,11 +6,17 @@ def parse_args():
     parser.add_argument('--input_bed')
     parser.add_argument('--desired_length',type=int)
     parser.add_argument('--output_bed')
+    parser.add_argument("--chromsizes")
     return parser.parse_args() 
 def main():
     args=parse_args()
     outf=open(args.output_bed,'w')
     data=open(args.input_bed,'r').read().strip().split('\n')
+    chromsizes=open(args.chromsizes,'r').read().strip().split('\n')
+    chromsize_dict=dict()
+    for line in chromsizes:
+        tokens=line.split('\t') 
+        chromsize_dict[tokens[0]]=int(tokens[1]) 
     for line in data:
         tokens=line.split('\t')
         chrom=tokens[0]
@@ -20,7 +26,14 @@ def main():
         topad=args.desired_length-peak_width
         startpos-=int(math.floor(topad/2))
         endpos+=int(math.ceil(topad/2))
-        outf.write(chrom+'\t'+str(startpos)+'\t'+str(endpos)+'\n')
+        if startpos < 1:
+            startpos=1
+            endpos=startpos+args.desired_length-1
+        if endpos> chromsize_dict[chrom]:
+            print("skipping line due to invalid chromosome position:"+line)
+            continue
+        else: 
+            outf.write(chrom+'\t'+str(startpos)+'\t'+str(endpos)+'\n')
         
 if __name__=="__main__":
     main() 
