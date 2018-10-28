f44f229599a2b9b3e15547d0970590bce8905c46
annashcherbina <annashcherbina@gmail.com>
2018-10-27 21:31:08 -0700

    add option to provide sheet names

--- io/merge_tsv_to_excel.py
+++ io/merge_tsv_to_excel.py
@@ -5,15 +5,21 @@ import argparse
 def parse_args():
     parser=argparse.ArgumentParser(description="merge multiple tsv or csv into 1 excel")
     parser.add_argument("--input_f",nargs="+")
-    parser.add_argument("--input_delim",default="\t") 
+    parser.add_argument("--input_delim",default="\t")
+    parser.add_argument("--sheet_names",nargs="+",default=None)
     parser.add_argument("--outf")
     return parser.parse_args()
 
 def main():
     args=parse_args() 
     workbook = xlsxwriter.Workbook(args.outf)
-    for filename in args.input_f: 
-        ws = workbook.add_worksheet(str(filename.split('/')[-1]))
+    for findex in range(len(args.input_f)):
+        filename=args.input_f[findex]
+        if args.sheet_names!=None:
+            cur_sheet_name=args.sheet_names[findex]
+        else:
+            cur_sheet_name=filename
+        ws = workbook.add_worksheet(cur_sheet_name)
         spamReader = csv.reader(open(filename, 'r'), delimiter=args.input_delim,quotechar='"')
         row_count = 0
         print(filename)
